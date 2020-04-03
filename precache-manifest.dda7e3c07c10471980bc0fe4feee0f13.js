a70c2be94e18204fdf677d4e8f91b2ba6c68851f
Juan Camilo Gomez Lopez <juankg214@gmail.com>
2020-04-03 00:42:28 -0500

    Updates

---
+++ precache-manifest.dda7e3c07c10471980bc0fe4feee0f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a5c5a3087d52310242da118591e730",
+    "url": "/test_login/index.html"
+  },
+  {
+    "revision": "ee6b5e0ffe99fe3aed4b",
+    "url": "/test_login/static/css/main.f06aa00a.chunk.css"
+  },
+  {
+    "revision": "cbecb7c140d78c042310",
+    "url": "/test_login/static/js/2.66bbea70.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test_login/static/js/2.66bbea70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee6b5e0ffe99fe3aed4b",
+    "url": "/test_login/static/js/main.12e84016.chunk.js"
+  },
+  {
+    "revision": "60921919e289eade5081",
+    "url": "/test_login/static/js/runtime-main.ff81e355.js"
+  },
+  {
+    "revision": "69b727427fdbccbcecd6d54814479842",
+    "url": "/test_login/static/media/rabbit.69b72742.png"
+  }
+]);
